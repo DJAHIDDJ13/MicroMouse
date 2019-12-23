29240abaefd9e3fb1eadc773dcf4566a74181322
Donald LAY <dlay@admantic.fr>
2019-12-23 13:03:29 +0100

    Code skeleton

---
+++ MM_brain/communication.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "communication.h"
+#include "utils.h"
+
---
+++ MM_brain/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "utils.h"
+#include "communication.h"
+
+int main(int argc, char *argv[]) {
+
+
+    return 0;
+}
