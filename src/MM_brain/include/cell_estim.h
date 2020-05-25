2ee22b30a09d01c712cd2655cae8ee48472da549
Donald LAY <dlay@admantic.fr>
2020-05-25 15:51:56 +0200

    cell_estim : add wall_detect()

--- src/MM_brain/include/cell_estim.h
+++ src/MM_brain/include/cell_estim.h
@@ -3,6 +3,16 @@
 
 extern struct timeval cur_celltime, prevtime;
 
+enum wall_position{NoWall, WallTop, WallRight, WallBottom, WallLeft};
+
+typedef struct {
+   iVec2 cell_pos;
+   enum wall_position wall_pos;
+} WallPosition;
+
 void init_cell(struct Micromouse* status);
 void update_cell(struct Micromouse* status);
+WallPosition* detect_wall(struct Micromouse status);
+/* utils */
+float float_abs(float val);
 #endif
--- src/MM_brain/src/cell_estim.c
+++ src/MM_brain/src/cell_estim.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <sys/time.h>
+#include <math.h>
 
 #include <micromouse.h>
 #include <position.h>
 #include <communication.h>
+#include <cell_estim.h>
 
 // time vals to track the time step more accurately
 struct timeval cur_celltime, prevtime;
@@ -51,4 +53,64 @@ void update_cell(struct Micromouse* status)
    // get the cell estimation
    status->cur_cell.x = (status->cur_pose.pos.x - status->header_data.origin_x) / status->header_data.box_width;
    status->cur_cell.y = -(status->cur_pose.pos.y - status->header_data.origin_y) / status->header_data.box_height;
+}
+
+WallPosition* detect_wall(struct Micromouse status) {
+   WallPosition *wall_positions = malloc(sizeof(WallPosition) * 4);
+
+   int i = 0;
+   Vec2 source = { .x = 0, .y = 0 };
+   Vec2 target = { .x = 0, .y = 0 };
+   Vec2 relative_cell_pos;
+
+   WallPosition wall_position;
+
+   for (i = 0; i < NB_SENSOR; i++) {
+      if (status.sensor_data.sensors[i] > 0) {
+         /* Compute source position */
+         source.x = status.cur_pose.pos.x + status.header_data.sensors_position[i].x * cos(status.cur_pose.ang.z+M_PI) - status.header_data.sensors_position[i].y * sin(status.cur_pose.ang.z+M_PI);
+         source.y = status.cur_pose.pos.y + status.header_data.sensors_position[i].x * sin(status.cur_pose.ang.z+M_PI) + status.header_data.sensors_position[i].y * cos(status.cur_pose.ang.z+M_PI);
+         /* Compute sensor and wall intersection */
+         target.x = source.x + status.sensor_data.sensors[i] * (10.0/1024.0) * cos(status.header_data.sensors_position[i].z*(M_PI/180) + status.cur_pose.ang.z+M_PI);
+         target.y = source.y + status.sensor_data.sensors[i] * (10.0/1024.0) * sin(status.header_data.sensors_position[i].z*(M_PI/180) + status.cur_pose.ang.z+M_PI);
+         /* Add offset */
+         source.x = source.x - status.header_data.origin_x;
+         source.y = source.y - status.header_data.origin_y;
+
+         target.x = target.x - status.header_data.origin_x;
+         target.y = target.y - status.header_data.origin_y;
+         /* Compute cell */
+         relative_cell_pos.x = fmod(target.x, status.header_data.box_width);
+         relative_cell_pos.y = fmod(target.y, status.header_data.box_height);
+
+         wall_positions[i].cell_pos.x = (int)(target.x / status.header_data.box_width);
+         wall_positions[i].cell_pos.y = (int)-(target.y / status.header_data.box_height);
+         /* Compute wall position */
+         if (  relative_cell_pos.x > -relative_cell_pos.y && 
+               status.header_data.box_width - relative_cell_pos.x > -relative_cell_pos.y) {
+            wall_positions[i].wall_pos = WallTop;
+         } else if ( relative_cell_pos.x > -relative_cell_pos.y && 
+                     status.header_data.box_width - relative_cell_pos.x < -relative_cell_pos.y) {
+            wall_positions[i].wall_pos = WallRight;
+         } else if ( relative_cell_pos.x < -relative_cell_pos.y && 
+                     status.header_data.box_width - relative_cell_pos.x < -relative_cell_pos.y) {
+            wall_positions[i].wall_pos = WallBottom;
+         } else if ( relative_cell_pos.x < -relative_cell_pos.y && 
+                     status.header_data.box_width - relative_cell_pos.x > -relative_cell_pos.y) {
+            wall_positions[i].wall_pos = WallLeft;
+         }
+      } else {
+         wall_positions[i].cell_pos.x = -1;
+         wall_positions[i].cell_pos.y = -1;
+         wall_positions[i].wall_pos = NoWall;
+      }
+   }
+   return wall_positions;
+}
+
+float float_abs(float val) {
+   if (val < 0)
+      return -val;
+   else
+      return val;
 }
