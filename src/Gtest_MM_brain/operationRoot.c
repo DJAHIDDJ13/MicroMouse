86550f956df36a77d3d10150707f66cc4c7d8a8a
ishak96 <ishakayad96@outlook.fr>
2020-05-27 01:01:52 +0200

    Add Gtest to create unit tests for our MM brain main functions

---
+++ src/Gtest_MM_brain/operationRoot.c
@@ -0,0 +1,14 @@
+double add_numbers(const double f1, const double f2)
+{
+    return f1 + f2;
+}
+
+double subtract_numbers(const double f1, const double f2)
+{
+    return f1 - f2;
+}
+
+double multiply_numbers(const double f1, const double f2)
+{
+    return f1 * f2;
+}
--- src/UI_brain/README.md
+++
@@ -1,2 +0,0 @@
-This is the user interface for the MicroMouse it should be able to receive
-real-time data from MM, and display it in a user friendly manner.
