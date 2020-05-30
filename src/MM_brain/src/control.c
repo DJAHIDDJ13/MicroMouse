e1f9cc8a7a298eb11350dbbc9a0899b79fc3db3e
Djahid ABDELMOUMENE <djahidabdelmoumen@gmail.com>
2020-05-30 14:15:45 +0200

    Turn pid

--- src/MM_brain/src/control.c
+++ src/MM_brain/src/control.c
@@ -164,22 +164,139 @@ void fwd_PID(struct Micromouse* status)
 }
 
 
-void turn_PID_pos(struct Micromouse* status)
+void turn_PID_pos(struct Micromouse* status, int init)
 {
    printf("NEED TO TURN USING POS\n");
    // CHOICE HERE: EITHER (LEFT OR RIGHT OR FORWARD) OR (LEFT OR FORWARD) OR (RIGHT OR FORWARD)
    // keep checking for forward sensors while doing the turning
    // if forward found go to dist_turning state
+   static float old_err1 = 0.0, old_err2 = 0.0, cumul_err1 = 0.0, cumul_err2 = 0.0;
+
+   static float init_ang = 0;
+   if(init) {
+      init_ang = M_PI_2 * round(status->cur_pose.ang.z / (M_PI_2));
+      old_err1 = old_err2 = cumul_err1 = cumul_err2 = 0.0f;
+   }
    
-   control_state = DEFAULT;
+   float right_sensor = status->sensor_data.sensors[1],
+         left_sensor = status->sensor_data.sensors[2],
+         right_middle_sensor = status->sensor_data.sensors[0],
+         left_middle_sensor = status->sensor_data.sensors[3];
+
+   float ang_diff = M_PI_2 - (init_ang - status->cur_pose.ang.z);
+   printf("NEED TO GO BACK\n");
+   // NO CHOICE HERE
+   // Go backwards until threshold is cleared then
+   // Actually do the turning until it's over
+   // Exit condition in case there isn't really a dead end
+   /*if((left_middle_sensor > 320 || right_middle_sensor > 320) &&
+      (left_sensor > TURNING_LENGTH_THRESHOLD || right_sensor > TURNING_LENGTH_THRESHOLD)) 
+   {
+      control_state = TURN_DIST;
+   }*/
+   float ang_dist = fmin(fabs(2*M_PI - status->cur_pose.ang.z - init_ang), 
+                         fabs(status->cur_pose.ang.z - init_ang));
+   if(fabs(ang_dist) > 0.9 * M_PI_2 || ((left_middle_sensor > 800 || left_middle_sensor < 0) && (right_middle_sensor > 800 || right_middle_sensor < 0))) {
+      control_state = DEFAULT;
+   }
+   
+   float err1 = 0.0f, err2 = 0.0f;
+   float Kp = 1, Kd = 1000, Ki = 0.0f;
+  
+   // calculating the speed for each wheel 
+   Vec2 speed = {.x=status->cur_pose.pos.x - status->cur_pose.pos.x,
+                 .y=status->cur_pose.pos.y - status->cur_pose.pos.y};
+   speed.x = cos(status->cur_pose.ang.z) * speed.x - sin(status->cur_pose.ang.z) * speed.y;
+   speed.y = sin(status->cur_pose.ang.z) * speed.x + cos(status->cur_pose.ang.z) * speed.y;
+
+   if((left_middle_sensor < 250 || right_middle_sensor < 250) && 
+       left_middle_sensor > 0 && right_middle_sensor > 0) {
+      err1 = -right_middle_sensor/100;
+      err2 = -left_middle_sensor/100;
+   } else if((left_middle_sensor > 300 && right_middle_sensor > 300) || 
+             (fabs(speed.x) > 5 || fabs(speed.y) > 5)) {
+      err1 = -200*speed.x + right_middle_sensor/100;
+      err2 = -200*speed.y + left_middle_sensor/100;
+   } else {
+      Kd = 0;
+      err1 = 30 * ang_diff;
+      err2 = -30 * ang_diff;
+   }
+   // calling the general PID function   
+   float out1 = PID(status, err1, Kp, Ki, Kd, &old_err1, &cumul_err1);
+   float out2 = PID(status, err2, Kp, Ki, Kd, &old_err2, &cumul_err2);
+
+   status->engines[0] = out1;
+   status->engines[1] = out2; 
+  
+   // control_state = DEFAULT;
 }
 
-void turn_PID_dist(struct Micromouse* status)
+void turn_PID_dist(struct Micromouse* status, int init)
 {
    printf("NEED TO TURN USING DIST\n");
    // CHOICE HERE: EITHER (LEFT OR RIGHT) OR (LEFT) OR (RIGHT)
    // Actually do the turning
-   control_state = DEFAULT;
+   static float old_err1 = 0.0, old_err2 = 0.0, cumul_err1 = 0.0, cumul_err2 = 0.0;
+
+   static float init_ang = 0;
+   if(init) {
+      init_ang = M_PI_2 * round(status->cur_pose.ang.z / (M_PI_2));
+      old_err1 = old_err2 = cumul_err1 = cumul_err2 = 0.0f;
+   }
+   
+   float right_sensor = status->sensor_data.sensors[1],
+         left_sensor = status->sensor_data.sensors[2],
+         right_middle_sensor = status->sensor_data.sensors[0],
+         left_middle_sensor = status->sensor_data.sensors[3];
+
+   float ang_diff = M_PI_2 - (init_ang - status->cur_pose.ang.z);
+   printf("NEED TO GO BACK\n");
+   // NO CHOICE HERE
+   // Go backwards until threshold is cleared then
+   // Actually do the turning until it's over
+   // Exit condition in case there isn't really a dead end
+   /*if((left_middle_sensor > 320 || right_middle_sensor > 320) &&
+      (left_sensor > TURNING_LENGTH_THRESHOLD || right_sensor > TURNING_LENGTH_THRESHOLD)) 
+   {
+      control_state = TURN_DIST;
+   }*/
+   float ang_dist = fmin(fabs(2*M_PI - status->cur_pose.ang.z - init_ang), 
+                         fabs(status->cur_pose.ang.z - init_ang));
+   if(fabs(ang_dist) > 0.9 * M_PI_2 || ((left_middle_sensor > 800 || left_middle_sensor < 0) && (right_middle_sensor > 800 || right_middle_sensor < 0))) {
+      control_state = DEFAULT;
+   }
+   
+   float err1 = 0.0f, err2 = 0.0f;
+   float Kp = 1, Kd = 1000, Ki = 0.0f;
+  
+   // calculating the speed for each wheel 
+   Vec2 speed = {.x=status->cur_pose.pos.x - status->cur_pose.pos.x,
+                 .y=status->cur_pose.pos.y - status->cur_pose.pos.y};
+   speed.x = cos(status->cur_pose.ang.z) * speed.x - sin(status->cur_pose.ang.z) * speed.y;
+   speed.y = sin(status->cur_pose.ang.z) * speed.x + cos(status->cur_pose.ang.z) * speed.y;
+
+   if((left_middle_sensor < 250 || right_middle_sensor < 250) && 
+       left_middle_sensor > 0 && right_middle_sensor > 0) {
+      err1 = -right_middle_sensor/100;
+      err2 = -left_middle_sensor/100;
+   } else if((left_middle_sensor > 300 && right_middle_sensor > 300) || 
+             (fabs(speed.x) > 5 || fabs(speed.y) > 5)) {
+      err1 = -200*speed.x + right_middle_sensor/100;
+      err2 = -200*speed.y + left_middle_sensor/100;
+   } else {
+      Kd = 0;
+      err1 = 30 * ang_diff;
+      err2 = -30 * ang_diff;
+   }
+   // calling the general PID function   
+   float out1 = PID(status, err1, Kp, Ki, Kd, &old_err1, &cumul_err1);
+   float out2 = PID(status, err2, Kp, Ki, Kd, &old_err2, &cumul_err2);
+
+   status->engines[0] = out1;
+   status->engines[1] = out2; 
+    
+   // control_state = DEFAULT;
 }
 
 void update_control(struct Micromouse* status, char init)
@@ -202,11 +319,11 @@ void update_control(struct Micromouse* status, char init)
       return;
 
    case TURN_POS:
-      turn_PID_pos(status);
+      turn_PID_pos(status, 0);
       return;
 
    case TURN_DIST:
-      turn_PID_dist(status);
+      turn_PID_dist(status, 0);
       return;
 
    default:
@@ -219,12 +336,12 @@ void update_control(struct Micromouse* status, char init)
 
    {
       control_state = TURN_DIST;
-      turn_PID_dist(status);
+      turn_PID_dist(status, 1);
    } else if((status->sensor_data.sensors[0] < 0 || status->sensor_data.sensors[3] < 0) &&
              ((status->sensor_data.sensors[1] < 0 && status->sensor_data.sensors[2] > 0) ||
               (status->sensor_data.sensors[1] > 0 && status->sensor_data.sensors[2] < 0))) {
       control_state = TURN_POS;
-      turn_PID_pos(status);
+      turn_PID_pos(status, 1);
    } else if(status->sensor_data.sensors[0] > 0 &&// status->sensor_data.sensors[0] < 700 &&
              status->sensor_data.sensors[1] > 0 &&// status->sensor_data.sensors[1] < 700 &&
              status->sensor_data.sensors[2] > 0 &&// status->sensor_data.sensors[2] < 700 &&
